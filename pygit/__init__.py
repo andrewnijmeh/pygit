3680242054dddb0ce20e732f0ff510e59b3d2381
andrew <andrewnijmeh1@gmail.com>
2020-11-29 20:39:14 -0500

    update

--- pygit/__init__.py
+++ pygit/__init__.py
@@ -4,3 +4,4 @@ Github API wrapper
 A basic wrapper for the Github API
 :license: MIT, see LICENSE.md for more details.
 """
+
--- pygit/run_commands.py
+++
@@ -1,6 +0,0 @@
-import os 
-import sys
-import logging
-
-def run_commands(commands, error_message):
-    # Args: command  (list of strs): Commands to run. Error_message (string): Error message
